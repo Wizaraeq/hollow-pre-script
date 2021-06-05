3551f02b65c11c275a111ea758d9b2e92240f09b
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-05 03:15:23 +0000

    Replace c100278030.lua

--- c100278030.lua
+++ c100278030.lua
@@ -1,78 +1,105 @@
---サイレンス・シーネットル
-function c100278029.initial_effect(c)
-	--special summon
+--No.4 猛毒刺胞ステルス・クラーゲン
+function c100278030.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER),4,2)
+	c:EnableReviveLimit()
+	--attribute
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100278029,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,100278029)
-	e1:SetCondition(c100278029.spcon)
-	e1:SetCost(c100278029.spcost)
-	e1:SetTarget(c100278029.sptg)
-	e1:SetOperation(c100278029.spop)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsFaceup))
+	e1:SetValue(ATTRIBUTE_WATER)
 	c:RegisterEffect(e1)
-	--todeck
+	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100278029,1))
-	e2:SetCategory(CATEGORY_TODECK)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,100278129)
-	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(c100278029.tdtg)
-	e2:SetOperation(c100278029.tdop)
+	e2:SetDescription(aux.Stringid(100278030,0))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100278030.descon)
+	e2:SetTarget(c100278030.destg)
+	e2:SetOperation(c100278030.desop)
 	c:RegisterEffect(e2)
-	Duel.AddCustomActivityCounter(100278029,ACTIVITY_SPSUMMON,c100278029.counterfilter)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100278030,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(c100278030.spcon)
+	e3:SetTarget(c100278030.sptg)
+	e3:SetOperation(c100278030.spop)
+	c:RegisterEffect(e3)
 end
-function c100278029.counterfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER)
+c100278030.xyz_number=4
+function c100278030.descon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
 end
-function c100278029.cfilter(c)
+function c100278030.desfilter(c)
 	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
 end
-function c100278029.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c100278029.cfilter,tp,LOCATION_MZONE,0,1,nil)
+function c100278030.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100278030.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(c100278030.desfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
-function c100278029.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(100278029,tp,ACTIVITY_SPSUMMON)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTarget(c100278029.splimit)
-	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
-function c100278029.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsAttribute(ATTRIBUTE_WATER)
-end
-function c100278029.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function c100278029.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+function c100278030.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local tc=Duel.SelectMatchingCard(tp,c100278030.desfilter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Damage(1-tp,math.ceil(tc:GetAttack()/2),REASON_EFFECT)
 	end
 end
-function c100278029.tdfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToDeck()
+function c100278030.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=c:GetOverlayCount()
+	e:SetLabel(ct)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_XYZ) and ct>0
 end
-function c100278029.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100278029.tdfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100278029.tdfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,c100278029.tdfilter,tp,LOCATION_GRAVE,0,1,3,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+function c100278030.spfilter(c,e,tp)
+	return c:IsCode(100278031) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100278029.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	if g:GetCount()>0 then
-		Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+function c100278030.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_XYZ)>0
+		and Duel.IsExistingMatchingCard(c100278030.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100278030.matfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsCanOverlay()
+end
+function c100278030.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_XYZ)
+	if ft==0 then return end
+	ft=math.min(ft,e:GetLabel())
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+	if ect~=nil then ft=math.min(ft,ect) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100278030.spfilter,tp,LOCATION_EXTRA,0,1,ft,nil,e,tp)
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_MZONE)
+		local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100278030.matfilter),tp,LOCATION_GRAVE,0,nil)
+		local res=false
+		local tc=og:GetFirst()
+		while og do
+			if sg:GetCount()>0 then return end
+			if Duel.SelectEffectYesNo(tp,tc,aux.Stringid(100278030,2)) then
+				if res==false then
+					res=true
+					Duel.BreakEffect()
+				end
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+				local tg=sg:Select(tp,1,1,nil)
+				Duel.Overlay(tc,tg)
+				sg:Sub(tg)
+			end
+			tc=og:GetNext()
+		end
 	end
 end
