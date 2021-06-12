3ea8d733b3a99af65cb6e782e6938aef550a3e49
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-12 09:33:02 +0000

    git,未対応のIsPreviousControler

--- c101106026.lua
+++ c101106026.lua
@@ -1,19 +1,23 @@
 --ペンギン僧侶
+--
+--Script by mercury233
 function c101106026.initial_effect(c)
-	--Special summon
+	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101106026,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,101106026)
-	e1:SetTarget(c101106026.sstg)
-	e1:SetOperation(c101106026.ssop)
+	e1:SetCondition(c101106026.spcon)
+	e1:SetTarget(c101106026.sptg)
+	e1:SetOperation(c101106026.spop)
 	c:RegisterEffect(e1)
-	--gains ATK,LP
+	--atk up
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101106026,1))
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_RECOVER)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -23,28 +27,32 @@ function c101106026.initial_effect(c)
 	e2:SetOperation(c101106026.atkop)
 	c:RegisterEffect(e2)
 end
-function c101106026.cfilter(c,e,tp)
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsSetCard(0x5a)
-		and c:GetReasonPlayer()==1-tp and c:GetPreviousControler()==tp and c:IsCanBeEffectTarget(e)
+function c101106026.cfilter(c,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x5a)
+		and c:IsControler(tp) and c:GetReasonPlayer()==1-tp
+		and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
 end
-function c101106026.sstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return c101106026.cfilter(chkc,e,tp) end
-	if chk==0 then return eg:IsExists(c101106026.cfilter,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+function c101106026.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101106026.cfilter,1,nil,tp)
+end
+function c101106026.spfilter(c,e,tp,g)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) and g:IsContains(c)
+end
+function c101106026.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=eg:Filter(c101106026.cfilter,nil,tp)
+	if chkc then return c101106026.spfilter(chkc,e,tp,g) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101106026.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,g) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=eg:Filter(c101106026.cfilter,nil,e,tp)
-	local c=nil
-	if #g>1 then
-		c=g:Select(tp,1,1,nil):GetFirst()
-	else
-		c=g:GetFirst()
-	end
-	Duel.SetTargetCard(c)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,LOCATION_GRAVE)
+	local sg=Duel.SelectTarget(tp,c101106026.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,g)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,e:GetHandler(),1,0,0)
 end
-function c101106026.ssop(e,tp,eg,ep,ev,re,r,rp)
+function c101106026.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if Duel.SendtoGrave(c,REASON_EFFECT+REASON_DISCARD)==1 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc and tc:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and Duel.SendtoGrave(c,REASON_EFFECT+REASON_DISCARD)~=0
+		and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
 	end
 end
@@ -52,7 +60,7 @@ function c101106026.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x5a)
 end
 function c101106026.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101106026.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101106026.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c101106026.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,c101106026.filter,tp,LOCATION_MZONE,0,1,1,nil)
@@ -61,17 +69,16 @@ function c101106026.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,600)
 end
 function c101106026.atkop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetValue(600)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 		Duel.Recover(p,d,REASON_EFFECT)
-	end 
+	end
 end
--- c101106063.lua
+++ c101106063.lua
@@ -1,15 +1,18 @@
 --ロイヤル・ペンギンズ・ガーデン
+--
+--Script by mercury233
 function c101106063.initial_effect(c)
-	-- Activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,101106063,EFFECT_COUNT_CODE_OATH)
+	e1:SetCountLimit(1,101106063+EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(c101106063.activate)
 	c:RegisterEffect(e1)
-	-- Reduce level then discard
+	--lv down
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101106063,1))
 	e2:SetCategory(CATEGORY_HANDES)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
@@ -22,9 +25,8 @@ function c101106063.thfilter(c)
 	return c:IsSetCard(0x5a) and not c:IsCode(101106063) and c:IsAbleToHand()
 end
 function c101106063.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(c101106063.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(101106063,0)) then
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101106063,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
@@ -32,37 +34,32 @@ function c101106063.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c101106063.lvfilter(c)
-	return c:IsSetCard(0x5a) and c:IsLevelAbove(1) and not (c:IsLocation(LOCATION_MZONE) and c:IsFacedown())
+	return c:IsSetCard(0x5a) and c:IsLevelAbove(2) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
 end
 function c101106063.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.IsExistingMatchingCard(c101106063.lvfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
-			and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil,REASON_EFFECT+REASON_DISCARD)
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(c101106063.lvfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
+		and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
 function c101106063.lvop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101106063,1))
-	local g=Duel.SelectMatchingCard(tp,c101106063.lvfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
-	if #g>0 then
-		local tc=g:GetFirst()
+	local g=Duel.GetMatchingGroup(c101106063.lvfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+	if g:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101106063,2))
+		local sg=g:Select(tp,1,1,nil)
+		local tc=sg:GetFirst()
 		if tc:IsLocation(LOCATION_MZONE) then
-			Duel.HintSelection(g)
+			Duel.HintSelection(sg)
 		else
-			Duel.ConfirmCards(1-tp,g)
+			Duel.ConfirmCards(1-tp,sg)
 			Duel.ShuffleHand(tp)
 		end
-		-- Reduce level
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_LEVEL)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
 		e1:SetValue(-1)
 		tc:RegisterEffect(e1)
-		if Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil,REASON_EFFECT+REASON_DISCARD) then
-			Duel.BreakEffect()
-			Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_EFFECT+REASON_DISCARD)
-		end
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,nil,1,1,REASON_DISCARD+REASON_EFFECT,nil)
 	end
 end
