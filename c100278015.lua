121ec6fc396c035e67dbf466be2af26430af2558
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-12 06:23:33 +0000

    git

--- c100278015.lua
+++ c100278015.lua
@@ -1,6 +1,8 @@
 --サモン・ストーム
+--
+--Script by 魔方
 function c100278015.initial_effect(c)
-	--Special summon 1 level 6 or lower WIND monster from hand
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100278015,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -10,15 +12,15 @@ function c100278015.initial_effect(c)
 	e1:SetTarget(c100278015.target)
 	e1:SetOperation(c100278015.activate)
 	c:RegisterEffect(e1)
-	--Special summon 1 level 4 or lower WIND monster from hand
+	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100278015,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,100278015)
-	e2:SetCost(aux.bfgcost)
 	e2:SetCondition(aux.exccon)
+	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c100278015.sptg)
 	e2:SetOperation(c100278015.spop)
 	c:RegisterEffect(e2)
@@ -28,35 +30,34 @@ function c100278015.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.PayLPCost(tp,800)
 end
 function c100278015.filter(c,e,tp)
-	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
-end
-function c100278015.spfilter1(c,e,tp)
-	return c100278015.filter(c,e,tp) and c:IsLevelBelow(6)
-end
-function c100278015.spfilter2(c,e,tp)
-	return c100278015.filter(c,e,tp) and c:IsLevelBelow(4)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(6) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100278015.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c100278015.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100278015.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function c100278015.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100278015.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 then
+	local g=Duel.SelectMatchingCard(tp,c100278015.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+function c100278015.spfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function c100278015.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c100278015.spfilter2,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100278015.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function c100278015.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100278015.spfilter2,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 then
+	local g=Duel.SelectMatchingCard(tp,c100278015.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
