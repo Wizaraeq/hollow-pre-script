4d3e7948ad1366e70ae6614274c0bfee8fbc3d63
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-12 08:54:52 +0000

    git,未対応のIsSummonLocation

--- c100278008.lua
+++ c100278008.lua
@@ -1,49 +1,51 @@
 --ペンギン・ソード
+--
+--Script by mercury233
 function c100278008.initial_effect(c)
 	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetCategory(CATEGORY_EQUIP)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	e0:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CONTINUOUS_TARGET)
-	e0:SetTarget(c100278008.target)
-	e0:SetOperation(c100278008.operation)
-	c:RegisterEffect(e0)
-	--Equip limit
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_EQUIP_LIMIT)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetValue(c100278008.eqlimit)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CONTINUOUS_TARGET)
+	e1:SetTarget(c100278008.target)
+	e1:SetOperation(c100278008.operation)
 	c:RegisterEffect(e1)
-	--Atk up
+	--equip limit
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(800)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetValue(c100278008.eqlimit)
 	c:RegisterEffect(e2)
-	--Return
+	--atk up
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_BATTLE_DAMAGE)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(c100278008.retcon)
-	e3:SetTarget(c100278008.rettg)
-	e3:SetOperation(c100278008.retop)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(800)
 	c:RegisterEffect(e3)
-	--Negate
+	--special summon
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e4:SetCode(EVENT_TO_HAND)
+	e4:SetDescription(aux.Stringid(100278008,0))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BATTLE_DAMAGE)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCountLimit(1,100278008)
-	e4:SetTarget(c100278008.negtg)
-	e4:SetOperation(c100278008.negop)
+	e4:SetCondition(c100278008.rthcon)
+	e4:SetTarget(c100278008.rthtg)
+	e4:SetOperation(c100278008.rthop)
 	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EVENT_REMOVE)
+	--disable
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100278008,1))
+	e5:SetCategory(CATEGORY_DISABLE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_LEAVE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,100278008)
+	e5:SetCondition(c100278008.negcon)
+	e5:SetOperation(c100278008.negop)
 	c:RegisterEffect(e5)
 end
 function c100278008.eqlimit(e,c)
@@ -53,75 +55,81 @@ function c100278008.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x5a)
 end
 function c100278008.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100278008.filter(chkc) end
+	if chkc then return chkc:GetLocation()==LOCATION_MZONE and c100278008.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c100278008.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	Duel.SelectTarget(tp,c100278008.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 function c100278008.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,e:GetHandler(),tc)
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
 	end
 end
-function c100278008.retcon(e,tp,eg,ep,ev,re,r,rp)
+function c100278008.rthcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp and eg:GetFirst()==e:GetHandler():GetEquipTarget()
 end
-function c100278008.thfilter(c)
+function c100278008.rthfilter(c)
 	return c:IsAbleToHand() and not c:IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function c100278008.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100278008.thfilter,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_ONFIELD)
+function c100278008.rthtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100278008.rthfilter,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
 end
-function c100278008.retop(e,tp,eg,ep,ev,re,r,rp)
+function c100278008.rthop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100278008.thfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
+	local g=Duel.SelectMatchingCard(tp,c100278008.rthfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
 end
-function c100278008.cfilter(c,tp,re)
-	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetPreviousControler()==1-tp
-		and c:IsReason(REASON_EFFECT) and re:GetHandler():IsSetCard(0x5a)
+function c100278008.cfilter(c,tp)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
+		and c:IsReason(REASON_EFFECT) and c:IsLocation(LOCATION_HAND+LOCATION_REMOVED)
 end
-function c100278008.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg and eg:IsExists(c100278008.cfilter,1,nil,tp,re) end
+function c100278008.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:GetHandler():IsSetCard(0x5a) and eg:IsExists(c100278008.cfilter,1,nil,tp)
 end
 function c100278008.negop(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(c100278008.cfilter,nil,tp,re)
-	local tc=g:GetFirst()
+	local c=e:GetHandler()
+	local g=eg:Filter(c100278008.cfilter,nil,tp)
 	for tc in aux.Next(g) do
-		local c=e:GetHandler()
-		local code=tc:GetOriginalCode()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_DISABLE)
 		e1:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
-		e1:SetLabel(code)
 		e1:SetTarget(c100278008.distg)
+		e1:SetLabelObject(tc)
 		e1:SetReset(RESET_PHASE+PHASE_END,2)
 		Duel.RegisterEffect(e1,tp)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e2:SetCode(EVENT_CHAIN_SOLVING)
-		e2:SetLabel(code)
 		e2:SetCondition(c100278008.discon)
 		e2:SetOperation(c100278008.disop)
+		e2:SetLabelObject(tc)
 		e2:SetReset(RESET_PHASE+PHASE_END,2)
 		Duel.RegisterEffect(e2,tp)
-		local e3=e1:Clone()
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD)
 		e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
 		e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e3:SetTarget(c100278008.distg)
+		e3:SetLabelObject(tc)
+		e3:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e3,tp)
 	end
 end
 function c100278008.distg(e,c)
-	return c:IsOriginalCodeRule(e:GetLabel())
+	local tc=e:GetLabelObject()
+	return c:IsOriginalCodeRule(tc:GetOriginalCodeRule())
 end
 function c100278008.discon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler():IsOriginalCodeRule(e:GetLabel())
+	local tc=e:GetLabelObject()
+	return re:GetHandler():IsOriginalCodeRule(tc:GetOriginalCodeRule())
 end
 function c100278008.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
