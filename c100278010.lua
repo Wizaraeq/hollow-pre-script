fcb50897583508bf6e0b3e28f4c263a8edfebf23
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-14 06:15:52 +0000

    未対応のCATEGORY_GRAVE_SPSUMMON

--- c100278010.lua
+++ c100278010.lua
@@ -5,7 +5,7 @@ function c100278010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100278010,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_ACTION+CATEGORY_GRAVE_SPSUMMON)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_ACTION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(c100278010.cost)
@@ -47,6 +47,8 @@ function c100278010.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		return res or Duel.IsExistingMatchingCard(c100278010.thfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	end
 	e:SetLabel(0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function c100278010.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
