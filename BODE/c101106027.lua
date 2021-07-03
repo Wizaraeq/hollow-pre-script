fcd2275cf251a22dc66df111728835870505bb7c
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-07-03 13:08:53 +0000

    add

---
+++ BODE/c101106027.lua
@@ -0,0 +1,70 @@
+--ホーリーナイツ・オルビタエル
+function c101106027.initial_effect(c)
+	--set
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101106027,0))
+	e1:SetCategory(CATEGORY_RELEASE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101106027)
+	e1:SetTarget(c101106027.settg)
+	e1:SetOperation(c101106027.setop)
+	c:RegisterEffect(e1)
+	--Special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101106027,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_HAND)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,101106027+100)
+	e2:SetCondition(c101106027.spcon)
+	e2:SetTarget(c101106027.sptg)
+	e2:SetOperation(c101106027.spop)
+	c:RegisterEffect(e2)
+end
+function c101106027.rfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsReleasableByEffect()
+end
+function c101106027.setfilter(c)
+	return c:IsSetCard(0x159) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
+end
+function c101106027.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c101106027.rfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101106027.rfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c101106027.setfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c101106027.rfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,g,1,0,0)
+end
+function c101106027.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Release(tc,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local g=Duel.SelectMatchingCard(tp,c101106027.setfilter,tp,LOCATION_DECK,0,1,1,nil,false)
+		if #g>0 then
+			Duel.SSet(tp,g:GetFirst())
+		end
+	end
+end
+function c101106027.spcfilter(c,tp)
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE) and c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevel(7) and c:IsPreviousPosition(POS_FACEUP)
+end
+function c101106027.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101106027.spcfilter,1,nil,tp) and not eg:IsContains(e:GetHandler())
+end
+function c101106027.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,LOCATION_GRAVE)
+end
+function c101106027.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
