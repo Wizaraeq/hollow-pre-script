a2e7e03824ae88a51ef2c74b71624da38329df86
ｔｈｒ <wizcab@gmail.com>
2021-07-12 22:01:45 +0900

    git

--- BODE/c101106051.lua
+++ BODE/c101106051.lua
@@ -30,7 +30,6 @@ function c101106051.initial_effect(c)
 	e2:SetOperation(c101106051.tgop)
 	c:RegisterEffect(e2)
 end
-c101106051.rchecks=aux.CreateChecks(Card.IsSetCard,{0x152,0x153})
 function c101106051.lcheck(g,lc)
 	return g:IsExists(Card.IsLinkSetCard,1,nil,0x2151)
 end
@@ -39,50 +38,50 @@ function c101106051.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c101106051.spfilter(c,e,tp)
-	return c:IsSetCard(0x152,0x153) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x153,0x152) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101106051.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101106051.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
+		and Duel.IsExistingMatchingCard(c101106051.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function c101106051.fselect(sg)
-	if #sg==1 then
-		return true
-	elseif #sg==2 then
-		return sg:CheckSubGroupEach(c101106051.rchecks)
-	end
+function c101106051.gcheck(g)
+	if #g==1 then return true end
+	return aux.gfcheck(g,Card.IsSetCard,0x153,0x152)
 end
 function c101106051.spop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=math.min((Duel.GetLocationCount(tp,LOCATION_MZONE)),2)
 	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101106051.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp)
-	if ft<=0 or g:GetCount()==0 then return end
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=g:SelectSubGroup(tp,c101106051.fselect,false,1,ft)
-	if sg and sg:GetCount()>0 then
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft>0 and #g>0 then
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end 
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:SelectSubGroup(tp,c101106051.gcheck,false,1,math.min(2,ft))
+		if sg then
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end
 function c101106051.costfilter(c,tp)
 	return c:IsSetCard(0x2151) and c:IsType(TYPE_MONSTER) and (c:IsLocation(LOCATION_HAND+LOCATION_DECK) or c:IsFaceup()) 
 		and c:IsAbleToGraveAsCost()
-		and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
 end
 function c101106051.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(c101106051.costfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,nil,tp) end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost()
+		and Duel.IsExistingMatchingCard(c101106051.costfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c101106051.costfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function c101106051.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 end
 function c101106051.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.HintSelection(g)
 		Duel.SendtoGrave(g,REASON_EFFECT)
