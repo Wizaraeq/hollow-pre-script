fc597829bab1b33f173cf094c7b0f61ff1bacebd
ｔｈｒ <wizcab@gmail.com>
2021-07-21 14:33:31 +0900

    git

--- BODE/c101106032.lua
+++ BODE/c101106032.lua
@@ -1,8 +1,9 @@
 --重起士道－ゴルドナイト
+
+--Script by Chrono-Genex
 function c101106032.initial_effect(c)
-	--Gemini status
 	aux.EnableDualAttribute(c)
-	--Search
+	--to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101106032,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -20,20 +21,25 @@ function c101106032.initial_effect(c)
 	--change race
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_CHANGE_RACE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_CHANGE_RACE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCondition(aux.IsDualState)
 	e3:SetValue(RACE_MACHINE)
 	c:RegisterEffect(e3)
-	--atk up
+	--atkup
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_UPDATE_ATTACK)
 	e4:SetValue(500)
 	c:RegisterEffect(e4)
 end
+if Auxiliary.IsTypeInText==nil then
+	function Auxiliary.IsTypeInText(c,type)
+		return c.has_text_type and type&c.has_text_type==type
+	end
+end
 function c101106032.thfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsCode(9848939,16146511,16984449,18096222,18993198,19041767,25669282,26120084,27979109,32271987,33846209,38026562,44088292,54485355,55100740,57441100,64463828,65959844,67045174,68366996,69488544,72631243,73567374,80476891,80758812,81599449,81601517,90965652,91798373,95750695,96029574) and c:IsAbleToHand()
+	return aux.IsTypeInText(c,TYPE_DUAL) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
 end
 function c101106032.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101106032.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -42,7 +48,7 @@ end
 function c101106032.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c101106032.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
+	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
