8042adabf8117dd10a1875037a1ba6eeabeeda21
Wizaraeq <wizackyank@gmail.com>
2023-10-14 15:00:24 +0900

    fix

--- AGOV-EN/c101202089.lua
+++ AGOV-EN/c101202089.lua
@@ -31,8 +31,8 @@ function s.initial_effect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_LEAVE_FIELD)
 	e3:SetCondition(s.spcon)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
@@ -73,7 +73,8 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetOverlayCount()>0
+	return c:IsReason(REASON_DESTROY) and c:GetReasonPlayer()==1-tp
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and c:GetOverlayCount()>0
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x1a4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
